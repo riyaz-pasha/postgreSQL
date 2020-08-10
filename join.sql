7887b92823985601f36e85d4f7ed4e01a08ded51
riyaz <riyazpasha880@gmail.com>
2020-08-10 22:40:17 +0530

    joined using inner and outer join

--- join.sql
+++ join.sql
@@ -5,3 +5,12 @@ SELECT *
 SELECT id, name, phoneNo, company.role, package
     FROM employee, company
     Where employee.role = company.role;
+
+SELECT *
+    FROM employee INNER JOIN company ON (employee.role=company.role);
+
+SELECT *
+    FROM employee LEFT OUTER JOIN company ON (employee.role=company.role);
+
+SELECT emp.id, emp.name, emp.phoneNo, company.role, package
+    FROM employee emp LEFT OUTER JOIN company ON (emp.role=company.role);
