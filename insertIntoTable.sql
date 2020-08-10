708807befa5431dedd5b8d8e77f70394d8477a70
riyaz <riyazpasha880@gmail.com>
2020-08-10 21:49:16 +0530

    insert into table

---
+++ insertIntoTable.sql
@@ -0,0 +1,8 @@
+INSERT  INTO employee 
+        VALUES('Riyaz', 12345 , 900009000);
+
+INSERT  INTO employee (name, id, phoneNo)
+        VALUES('Some A', 11111, 911119111);
+
+INSERT  INTO employee (id, phoneNo, name)
+        VALUES(2222, 922229222, 'Some B');
